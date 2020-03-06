1549150084519a2d647cb1322c6e316345d04346
vithya9750 <vithya97501@gmail.com>
2020-03-05 22:15:47 -0800

    project design

---
+++ src/test/java/demo.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class demo {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ELCOT\\eclipse-workspace\\Maven\\driver\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		
+
+	}
+
+}
